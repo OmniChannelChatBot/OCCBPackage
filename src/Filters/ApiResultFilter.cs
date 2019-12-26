07e1ad442c847ec962d2c9253443f5fcd3e32f36
Дмитрий Николаевич Вознесенский <kantuzzz@live.com>
2019-12-26 16:52:45 +0300

    first commit

---
+++ src/Filters/ApiResultFilter.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
+
+namespace OCCBPackage.Filters
+{
+    internal class ApiResultFilter : IAsyncResultFilter
+    {
+        public Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            switch (context.Result)
+            {
+                case NotFoundObjectResult nfor:
+                    context.HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+                    ResultApiProblemDetails(context, nfor.Value.ToString());
+                    break;
+
+                case BadRequestObjectResult bror:
+                    context.HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+                    ResultApiProblemDetails(context, bror.Value.ToString());
+                    break;
+
+                case NotFoundResult _:
+                    context.HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+                    ResultApiProblemDetails(context);
+                    break;
+            }
+
+            return next();
+        }
+
+        private void ResultApiProblemDetails(ResultExecutingContext resultExecutingContext, string title = default(string))
+        {
+            var apiProblemDetails = new ApiProblemDetails(resultExecutingContext.HttpContext, title);
+            var objectResult = new ObjectResult(apiProblemDetails)
+            {
+                StatusCode = resultExecutingContext.HttpContext.Response.StatusCode
+            };
+            resultExecutingContext.Result = objectResult;
+        }
+    }
+}
