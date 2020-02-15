7423cd19d8f5abf5ddd826bd08d03cdc591a005c
ZEXSM <zaxabor@gmail.com>
2020-02-15 13:33:36 +0300

    #ZEXSM#cookie auth#review

---
+++ src/OCCBPackage/Extensions/AuthenticationServiceCollectionExtensions.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using OCCBPackage.Mvc;
+using OCCBPackage.Options;
+using System;
+using System.Net.Mime;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace OCCBPackage.Extensions
+{
+    public static class AuthenticationServiceCollectionExtensions
+    {
+        public static AuthenticationBuilder AddJwtBearerAuthentication(this IServiceCollection services, Action<AccessTokenOptions> actionAccessTokenOptions, Action<RefreshTokenOptions> actionRefreshTokenOptions) => services
+            .Configure(actionRefreshTokenOptions)
+            .AddJwtBearerAuthentication(actionAccessTokenOptions);
+
+        public static AuthenticationBuilder AddJwtBearerAuthentication(this IServiceCollection services, Action<AccessTokenOptions> actionAccessTokenOptions) => services
+            .Configure(actionAccessTokenOptions)
+            .AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                var accessTokenOptions = new AccessTokenOptions();
+                actionAccessTokenOptions.Invoke(accessTokenOptions);
+
+                x.ClaimsIssuer = accessTokenOptions.Issuer;
+                x.RequireHttpsMetadata = accessTokenOptions.IsTransferTokenOverHttps;
+                x.SaveToken = accessTokenOptions.IsStoreTokenAfterAuthentication;
+                x.TokenValidationParameters = accessTokenOptions.GetTokenValidationParameters();
+                x.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = default(string);
+
+                        accessToken = context.Request.Cookies[AccessTokenOptions.TokenName];
+
+                        if (!string.IsNullOrEmpty(accessToken))
+                        {
+                            context.Token = accessToken;
+                            return Task.CompletedTask;
+                        }
+
+                        // TODO: When using WebSockets and server events, 
+                        //       the token is passed as a parameter of the query string with the access_token key
+                        //       https://docs.microsoft.com/ru-ru/aspnet/core/signalr/authn-and-authz?view=aspnetcore-3.1#bearer-token-authentication
+                        accessToken = context.Request.Query[AccessTokenOptions.TokenName].ToString();
+
+                        if (!string.IsNullOrEmpty(accessToken))
+                        {
+                            context.Token = accessToken;
+                            return Task.CompletedTask;
+                        }
+
+                        return Task.CompletedTask;
+                    },
+                    OnChallenge = context =>
+                    {
+                        if (context.AuthenticateFailure is SecurityTokenExpiredException)
+                        {
+                            context.Response.Headers.Add("Token-Expired", JwtBearerDefaults.AuthenticationScheme);
+                        }
+
+                        context.Response.Headers.Add("WWW-Authenticate", JwtBearerDefaults.AuthenticationScheme);
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+
+                        var response = JsonSerializer.Serialize(new ApiProblemDetails(context.HttpContext), new JsonSerializerOptions
+                        {
+                            IgnoreNullValues = true,
+                            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                        });
+
+                        return context.Response.WriteAsync(response);
+                    }
+                };
+            });
+    }
+}
--- src/OCCBPackage/Options/AccessTokenOptions.cs
+++ src/OCCBPackage/Options/AccessTokenOptions.cs
@@ -6,19 +6,25 @@ namespace OCCBPackage.Options
 {
     public class AccessTokenOptions
     {
+        public const string TokenName = "access_token";
+
         public string Secret { get; set; }
 
         public string Issuer { get; set; }
+
+        public string Audience { get; set; }
 
         public DateTime IssuedAt => DateTime.UtcNow;
 
         public DateTime NotBefore => DateTime.UtcNow;
 
-        public string Audience { get; set; }
-
         public DateTime Expires => IssuedAt.AddMinutes(ExpiresInMinutes);
 
         public double ExpiresInMinutes { get; set; } = 120.0;
+
+        public bool IsTransferTokenOverHttps { get; set; } = true;
+
+        public bool IsStoreTokenAfterAuthentication { get; set; }
 
         public TokenValidationParameters GetTokenValidationParameters(bool validateLifetime = true) =>
             new TokenValidationParameters
