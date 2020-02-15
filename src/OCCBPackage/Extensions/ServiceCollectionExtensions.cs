8fa681cbeed27538a956237345faf4bc4ff134ab
ZEXSM <zaxabor@gmail.com>
2020-02-15 20:48:00 +0300

    #ZEXSM#cors policy

--- src/OCCBPackage/Extensions/ServiceCollectionExtensions.cs
+++ src/OCCBPackage/Extensions/ServiceCollectionExtensions.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using OCCBPackage.Options;
 using System;
 
 namespace OCCBPackage.Extensions
 {
     public static class ServiceCollectionExtensions
     {
+        private static readonly string _corsPolicy = nameof(_corsPolicy);
+
         public static IServiceCollection AddCookiePolicy(this IServiceCollection services, Action<CookiePolicyOptions> actionCookiePolicyOptions) => services
             .Configure(actionCookiePolicyOptions);
+
+        public static IServiceCollection AddCorsPolicy(this IServiceCollection services, Action<CorsPolicyOptions> action)
+        {
+            var clientOriginPolicyOptions = new CorsPolicyOptions();
+            action.Invoke(clientOriginPolicyOptions);
+
+            services.AddCors(sa => sa
+                .AddPolicy(_corsPolicy, cpb =>
+                {
+                    cpb.WithOrigins(clientOriginPolicyOptions.Origins);
+                    cpb.WithMethods(clientOriginPolicyOptions.Methods);
+                    cpb.WithHeaders(clientOriginPolicyOptions.Headers);
+                    if (clientOriginPolicyOptions.IsCredentials)
+                    {
+                        cpb.AllowCredentials();
+                    }
+                }));
+
+            return services;
+        }
     }
 }
---
+++ src/OCCBPackage/Options/CorsPolicyOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace OCCBPackage.Options
+{
+    public class CorsPolicyOptions
+    {
+        public string[] Origins { get; set; }
+
+        public string[] Methods { get; set; }
+
+        public string[] Headers { get; set; }
+
+        public bool IsCredentials { get; set; }
+    }
+}
