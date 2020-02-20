a603816c9d936c7c10421cdab975b5e4f69d8c09
Дмитрий Николаевич Вознесенский <kantuzzz@live.com>
2020-02-20 14:27:39 +0300

    SentryEventProcessor

---
+++ src/OCCBPackage/Sentry/SentryEventProcessor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Sentry;
+using Sentry.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCCBPackage.Sentry
+{
+    public class SentryEventProcessor : ISentryEventProcessor
+    {
+        private readonly IHttpContextAccessor _httpContext;
+
+        public SentryEventProcessor(IHttpContextAccessor httpContext) => _httpContext = httpContext;
+
+        public SentryEvent Process(SentryEvent @event)
+        {
+            // Here I can modify the event, while taking dependencies via DI
+
+            @event.SetExtra("Service name", Environments.GetServiceName());
+            return @event;
+        }
+    }
+}
